b12796a6de8120bfe669cb3bf21d6bb7b6f10fba
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-10-10 20:16:54 +0530

    exhaustmap

--- src/app/components/observable-demo3/observable-demo3.ts
+++ src/app/components/observable-demo3/observable-demo3.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Observable, switchMap, map } from 'rxjs';
+import { Observable, switchMap, map, exhaustMap, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-observable-demo3',
@@ -14,7 +14,7 @@ import { Observable, switchMap, map } from 'rxjs';
   styleUrl: './observable-demo3.css'
 })
 export class ObservableDemo3 {
-searchResult$: Observable<any> | undefined;
+  searchResult$: Observable<any> | undefined;
   searchForm: any;
   constructor(private http: HttpClient) {
     this.searchForm = new FormGroup({
@@ -24,9 +24,24 @@ searchResult$: Observable<any> | undefined;
   ngOnInit() {
     this.searchResult$ = this.searchForm.get("searchField").valueChanges.pipe(
       switchMap((term) =>
-		  this.http.get<any>(`https://dummyjson.com/users/search?q=${term}`)),
+        this.http.get<any>(`https://dummyjson.com/users/search?q=${term}`)),
       map((response: any) =>
-		 response.users.length > 0 ? response.users : []
-    ));
+        response.users.length > 0 ? response.users : []
+      ));
+  }
+
+  @ViewChild('loginBtn') loginBtn:any;
+  
+  ngAfterViewInit() {
+    /* this.http.get('https://fakestoreapi.com/products').subscribe(response => {
+      console.log('loging successful')
+    }) */
+
+    fromEvent(this.loginBtn.nativeElement, 'click').pipe(exhaustMap((val) => {
+      return this.http.get('https://fakestoreapi.com/products')
+    })).subscribe({
+      next: (res) => console.log('Response:', res),
+      error: (err) => console.error('Error:', err),
+    })
   }
 }
